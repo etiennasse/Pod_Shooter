142b26d56c32b5d5c285a1409423e36454ef8779
B533P-03\533 <test@hotmail.com>
2018-11-05 16:46:51 -0500

    marche pas vrm

---
+++ Pod_Shooter/Assets/PlayerDamage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour {
+    playerlife playerLife;
+	// Use this for initialization
+    public void TakeDamage(int damage)
+    {
+        playerLife.TakeDamage(damage);
+    }
+
+	void Start () {
+        playerLife = GetComponent<playerlife>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Pod_Shooter/Assets/playerlife.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerlife : NetworkBehaviour {
+    [SerializeField]
+    public RectTransform healthbar;
+    public const int maxlife = 100;
+    private NetworkStartPosition[] spawnPoints;
+    // Use this for initialization
+
+    [SyncVar(hook ="UpdateLife")]
+    int currentLife = maxlife;
+
+	void Start () {
+		if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+
+        }
+	}
+
+
+	
+    public void TakeDamage(int damage)
+    {
+        if(isServer)
+        {
+            print("asdad");
+            currentLife -= damage;
+            if (currentLife <= 0)
+            {
+                currentLife = 0;
+                Die();
+            }
+            
+        }
+    }
+
+    private void UpdateLife(int newlife)
+    {
+        healthbar.sizeDelta = new Vector2(newlife, healthbar.sizeDelta.y);
+    }
+
+    void Die()
+    {
+        RpcRespawn();
+    }
+
+    [ClientRpc]
+    public void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            currentLife = maxlife;
+            Vector3 spawnPoint = Vector3.zero;
+            if (spawnPoint != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+            transform.position = spawnPoint;
+        }
+    }
+}
